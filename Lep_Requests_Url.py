ca0690a2b75a96f7bb87b9619ef41cc79c0cb686
unknown <qq365947335@hotmail.com>
2022-07-25 12:51:34 +0800

    update

--- Lep_Requests_Url.py
+++ Lep_Requests_Url.py
@@ -1,9 +1,33 @@
 import urllib
 
 
+def urlpath2FileName(path:str, ignore_query:bool=True):
+    '''将url的path转换为创建文件名可用的字符串'''
+    divch = '@'
+    replaces = {
+        '/': divch,
+        '\\': divch,
+        '?': '[w]',
+        '*': '[x]',
+        '|': '[i]',
+        '>': '[r]',
+        '<': '[l]',
+        '"': '[y]',
+    }
+    if not path:
+        return divch
+    res = ''
+    for ch in path:
+        if replaces.__contains__(ch):
+            res += replaces[ch]
+        else:
+            res += ch
+    return res
+
 def encodeUriComponent(url:str, safe:str='/'):
     return urllib.parse.quote(url, safe=safe)
     
 def decodeUriComponent():
     # todo
     pass
+
