fd45f742c2ff06573848a3d76b2c8b920834d83d
unknown <qq365947335@hotmail.com>
2022-07-25 13:55:54 +0800

    update

--- lep_time.py
+++ lep_time.py
@@ -1,35 +1,38 @@
 import time as Time
 
-# 毫秒级时间戳, acc=Accuracy=精度, 同javascript: new Date().getTime(), 如: 1657084042511
-def get_now_acc_msec()->int: return round(Time.time()*1000)
+def get_now_acc_msec()->int: 
+    '''# 毫秒级时间戳, acc=Accuracy=精度, 同javascript: new Date().getTime(), 如: 1657084042511'''
+    return round(Time.time()*1000)
 
-# 秒级时间戳, acc=Accuracy=精度, 如: c
-def get_now_acc_sec()->int: return round(Time.time())
+def get_now_acc_sec()->int: 
+    '''# 秒级时间戳, acc=Accuracy=精度, 如: c'''
+    return round(Time.time())
 
-# 得到当前时间戳, 如: 1657083246.6974413
-def get_now_origin()->float:  return Time.time()
+def get_now_origin()->float:  
+    '''# 得到当前时间戳, 如: 1657083246.6974413'''
+    return Time.time()
 
+def get_now_struct()->Time.struct_time: 
+    '''# 得到时间结构体, 如: time.struct_time(tm_year=2022, tm_mon=7, tm_mday=6, tm_hour=12, tm_min=56, tm_sec=39, tm_wday=2, tm_yday=187, tm_isdst=0)'''
+    return Time.localtime()
 
-# 得到时间结构体, 如: time.struct_time(tm_year=2022, tm_mon=7, tm_mday=6, tm_hour=12, tm_min=56, tm_sec=39, tm_wday=2, tm_yday=187, tm_isdst=0)
-def get_now_struct()->Time.struct_time: return Time.localtime()
+def get_now_asc()->str: 
+    '''# 得到时间字符串, 如: Wed Jul  6 12:57:56 2022'''    
+    return Time.asctime( Time.localtime() )
 
-# 得到时间字符串, 如: Wed Jul  6 12:57:56 2022
-def get_now_asc()->str: return Time.asctime( Time.localtime() )
-
-# 得到时间字符串, 如: 2022-07-06 13:02:37
 def get_now_format(formatstr:str=None)->str: 
+    '''# 得到时间字符串, 如: 2022-07-06 13:02:37'''
     if formatstr is None: formatstr = "%Y-%m-%d %H:%M:%S"
     return Time.strftime(formatstr, Time.localtime()) 
 
-# 格式化时间
 def time_format(time, formatstr:str): 
+    '''# 格式化时间'''
     if type(time) == Time.struct_time:
         return Time.strftime(formatstr,time)
     return Time.strftime(formatstr, time_2_struct(time))
 
-
-# 时间戳转 time.struct_time 类型
 def time_2_struct(time):
+    '''# 时间戳转 time.struct_time 类型'''
     tname = type(time).__name__
     if tname == 'int':
         l = len(str(time))
@@ -53,8 +56,8 @@ def time_2_struct(time):
     else:
         raise Exception('错误的输入')
     
-# 格式化时间的帮助
 def time_format_help():
+    '''# 格式化时间的帮助'''
     time = get_now_origin()
     print('时间戳 如:', time)
     time = Time.localtime(time)
